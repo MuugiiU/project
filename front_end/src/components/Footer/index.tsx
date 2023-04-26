9044a860bbe14131366cd57d63430c471ec6f0ed
Evening <evening@admins-iMac-2.local>
2023-04-26 14:51:28 +0800

    footer-responsive

--- front_end/src/components/Footer/index.tsx
+++ front_end/src/components/Footer/index.tsx
@@ -1,8 +1,14 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import {
+  Facebook,
+  Twitter,
+  Instagram,
+  YouTube,
+  Face,
+} from "@mui/icons-material";
 
 export default function Footer() {
   const [auth, setAuth] = React.useState(true);
@@ -21,34 +27,128 @@ export default function Footer() {
   };
 
   return (
-      <Box sx={{position:"static" ,bottom:0, background: 'linear-gradient(to right, #55A3DF,#4BA58C,#1FC4DC, #5ECDB1)',color:"white",paddingLeft:"10%",paddingRight:"10%",paddingTop:"30px",paddingBottom:"30px",marginTop:"50px",width:"100"}}>
-          <Typography variant='h3' sx={{textAlign:{xs:"center",sm:"start"}}}>
-            E.RENT
+    <Box
+      sx={{
+        position: "static",
+        bottom: 0,
+        background:
+          "linear-gradient(to right, #55A3DF,#4BA58C,#1FC4DC, #5ECDB1)",
+        color: "white",
+        paddingLeft: "10%",
+        paddingRight: "10%",
+        paddingTop: "30px",
+        paddingBottom: "30px",
+        marginTop: "50px",
+        width: "100",
+      }}
+    >
+      <Typography
+        variant="h3"
+        sx={{ textAlign: { xs: "center", sm: "start" } }}
+      >
+        E.RENT
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: { xs: "column", sm: "row" },
+          paddingTop: "20px",
+          textAlign: { xs: "center", sm: "start" },
+        }}
+      >
+        <Box sx={{}}>
+          <Typography variant="h4">Mail</Typography>
+          <Link
+            href="#"
+            sx={{ textDecoration: "none", paddingTop: "5px" }}
+            color="inherit"
+          >
+            e.rent@gmail.com
+          </Link>
+        </Box>
+        <Box
+          sx={{
+            marginLeft: { sm: "15%" },
+            display: "flex",
+            flexDirection: "column",
+            textAlign: { xs: "center", sm: "start" },
+          }}
+        >
+          <Typography variant="h4" sx={{ marginTop: { xs: "20px" } }}>
+            Social
           </Typography>
-          <Box sx={{display:"flex",flexDirection:{xs:"column",sm:"row"},paddingTop:"20px",textAlign:{xs:"center",sm:"start"}}}>
-            <Box sx={{}}>
-                <Typography variant="h4">Mail</Typography>
-                <Link href="#" sx={{textDecoration:"none",paddingTop:"10px"}} color="inherit">e.rent@gmail.com</Link>
-            </Box>
-            <Box sx={{marginLeft:{sm:"15%"},display:"flex",flexDirection:"column",textAlign:{xs:"center",sm:"start"}}}>
-                <Typography variant="h4">Social</Typography>
-                <Link href="#" sx={{textDecoration:"none",paddingTop:"10px"}} color="inherit">Facebook</Link>
-                <Link href="#" sx={{textDecoration:"none",paddingTop:"5px"}} color="inherit">Instagram</Link>
-                <Link href="#" sx={{textDecoration:"none",paddingTop:"5px"}} color="inherit">Twitter</Link>
-                <Link href="#" sx={{textDecoration:"none",paddingTop:"5px"}} color="inherit">YouTube</Link>
-            </Box>
-            <Box sx={{marginLeft:{sm:"15%"},textAlign:{xs:"center",sm:"start"}}}>
-                <Typography variant="h4" >Contacts</Typography>
-                <Typography sx={{paddingTop:"10px"}}>+976 98364566</Typography>
-                <Typography sx={{paddingTop:"5px"}}>+976 98364566</Typography>
-                <Typography sx={{paddingTop:"5px"}}>+976 98364566</Typography>
-            </Box>
-            <Box sx={{marginLeft:{sm:"15%"},display:"flex",flexDirection:"column"}}>
-                <Typography variant="h4">Help</Typography>
-                <Link href="#" sx={{textDecoration:"none",paddingTop:"10px"}} color="inherit">Түгээмэл асуулт & хариулт</Link>
-                <Link href="#" sx={{textDecoration:"none",paddingTop:"5px"}} color="inherit">Нууцлалын бодлого</Link>
-            </Box>
-          </Box>
+          <Link
+            href="#"
+            sx={{ textDecoration: "none", paddingTop: "5px" }}
+            color="inherit"
+          >
+            <Facebook />
+            Facebook
+          </Link>
+          <Link
+            href="#"
+            sx={{ textDecoration: "none", paddingTop: "5px" }}
+            color="inherit"
+          >
+            <Instagram />
+            Instagram
+          </Link>
+          <Link
+            href="#"
+            sx={{ textDecoration: "none", paddingTop: "5px" }}
+            color="inherit"
+          >
+            <Twitter />
+            Twitter
+          </Link>
+          <Link
+            href="#"
+            sx={{ textDecoration: "none", paddingTop: "5px" }}
+            color="inherit"
+          >
+            <YouTube />
+            YouTube
+          </Link>
+        </Box>
+        <Box
+          sx={{
+            marginLeft: { sm: "15%" },
+            textAlign: { xs: "center", sm: "start" },
+          }}
+        >
+          <Typography variant="h4" sx={{ marginTop: { xs: "20px" } }}>
+            Contacts
+          </Typography>
+          <Typography sx={{ paddingTop: "5px" }}>+976 98364566</Typography>
+          <Typography sx={{ paddingTop: "5px" }}>+976 98364566</Typography>
+          <Typography sx={{ paddingTop: "5px" }}>+976 98364566</Typography>
+        </Box>
+        <Box
+          sx={{
+            marginLeft: { sm: "15%" },
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Typography variant="h4" sx={{ marginTop: { xs: "20px" } }}>
+            Help
+          </Typography>
+          <Link
+            href="#"
+            sx={{ textDecoration: "none", paddingTop: "5px" }}
+            color="inherit"
+          >
+            Түгээмэл асуулт & хариулт
+          </Link>
+          <Link
+            href="#"
+            sx={{ textDecoration: "none", paddingTop: "5px" }}
+            color="inherit"
+          >
+            Нууцлалын бодлого
+          </Link>
+        </Box>
       </Box>
+    </Box>
   );
 }
